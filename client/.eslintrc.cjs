dba56bc647db8a026037ea10d6471231decf3fd8
CindyMisoi <crmisoi2k@gmail.com>
2024-06-05 13:58:11 +0300

    fix: lints

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,11 +1,36 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true
+  },
   extends: [
     'eslint:recommended',
-    // 'plugin:react/recommended',
+    'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
-  parserOptions: { ecmaVersion: '2020', sourceType: 'module' },
-  settings: { react: { version: '17.0.2' } },
+  parserOptions: {
+    ecmaVersion: '2020',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: '17.0.2'
+    }
+  },
+  plugins: ['testing-library'],
+  rules: {
+    'testing-library/no-node-access': 'warn',
+    'testing-library/no-container': 'warn',
+    'testing-library/no-debugging-utils': 'warn',
+    'testing-library/no-dom-import': 'off'
+  },
+  overrides: [
+    {
+      files: ['src/components/pages/**/*.test.js'],
+      rules: {
+        'testing-library/no-dom-import': 'off'
+      }
+    }
+  ]
 };
