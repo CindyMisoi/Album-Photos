7de2d717203d07db373e849d383cca879d4a4e0f
CindyMisoi <crmisoi2k@gmail.com>
2024-06-05 11:34:48 +0300

    fix: separated apiServer files

--- client/src/components/api/apiServer.jsx
+++ client/src/components/api/apiServer.jsx
@@ -1,13 +1,7 @@
-import axios from "axios";
+import createApiServer from "./createApiServer";
 
 const JwtToken = sessionStorage.getItem("Jwt_token"); // Replace with the actual session token value
 
-const apiServer = axios.create({
-  baseURL: "http://127.0.0.1:3000", // Replace with your Rails API server URL
-});
-
-if (JwtToken) {
-  apiServer.defaults.headers.common['Authorization'] = 'Bearer ' + JwtToken;
-}
+const apiServer = createApiServer(JwtToken);
 
 export default apiServer;
