3b8161f1371543f6f443b03afb910ad190b02403
CindyMisoi <crmisoi2k@gmail.com>
2024-06-05 19:05:59 +0300

    fix: Routing issue where controller actions dont match routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users do
+    resources :users, only: [:index, :show, :destroy] do
       collection do
         # Google signin
         post 'google', to: 'users#google'
       end
     end
     resources :sessions, only: [:create, :destroy]
-    resources :albums do 
-      collection do
-        # get all albums for a user
-        get '/user/:id', to: 'albums#get_ablbums_for_user'
-      end
+    resources :albums, only: [:index, :show] do
+      get 'get_albums_for_user', on: :collection
     end
-    resources :photos
+    resources :photos, only: [:index, :show, :update]
   end
 end
