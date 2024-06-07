b2ea2634d21e7625d25ee4ae36b7a9f29a1ab26f
CindyMisoi <crmisoi2k@gmail.com>
2024-06-07 13:57:26 +0300

    fix: routing error

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -9,10 +9,8 @@ Rails.application.routes.draw do
     end
     resources :sessions, only: [:create, :destroy]
     resources :albums, only: [:index, :show] do
-      root to: 'albums#index'
       get 'get_albums_for_user', on: :collection
     end
     resources :photos, only: [:index, :show, :update]
-    root to: 'photos#index'
   end
 end
