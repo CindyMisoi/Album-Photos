43989bee90e407afa1dfc6a77614a5b62735bed6
CindyMisoi <crmisoi2k@gmail.com>
2024-06-07 13:49:44 +0300

    fix: routing error

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
       collection do
         # Google signin
         post 'google', to: 'users#google'
+        root to: 'users#index'
       end
     end
     resources :sessions, only: [:create, :destroy]
     resources :albums, only: [:index, :show] do
+      root to: 'albums#index'
       get 'get_albums_for_user', on: :collection
     end
     resources :photos, only: [:index, :show, :update]
+    root to: 'photos#index'
   end
 end
